e2acab3d0c58492b0ed709dc9a78e95997d1a72b
Marco01373782 <116099860+Marco01373782@users.noreply.github.com>
2023-01-20 13:50:39 -0500

    Add files via upload

---
+++ calificaciones.js
@@ -0,0 +1,9 @@
+let califications=[60,87,68,79,45];
+for(let i=0; i<califications.length; i++){
+     if(califications[i]>=70){
+        console.log(califications[i]+"aprueba");
+    }
+    else{
+        console.log(califications[i]+"reprueba");
+    }
+} 
---
+++ suma.js
@@ -0,0 +1,4 @@
+let nums=[6,5,8,8,6,4,5,8,2,1,4,1,9,4,1];
+let sum = nums.reduce((accumulator, currentValue)=> accumulator+currentValue);{
+    console.log(sum);
+}
